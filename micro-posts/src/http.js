de194da230a5923a89fa1c903f41d11783e78699
David Cate <david@element-360.com>
2018-09-16 21:15:32 -0400

    microposts delete post update

---
+++ micro-posts/src/http.js
@@ -0,0 +1,49 @@
+class EasyHTTP {
+  // make an http get request
+  async get(url) {
+    const response = await fetch(url);
+    const resData = await response.json();
+    return resData;
+  }
+
+  // make an http post request
+  async post(url, data) {
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  // make an http put request
+  async put(url, data) {
+    const response = await fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+
+    const resData = await response.json();
+    return resData;
+  }
+
+  async delete(url) {
+    const response = await fetch(url, {
+      method: 'DELETE',
+      headers: {
+        'Content-type': 'application/json'
+      }
+    });
+    const resData = 'resource deleted...';
+    return resData;
+  }
+}
+
+export const http = new EasyHTTP(); 
